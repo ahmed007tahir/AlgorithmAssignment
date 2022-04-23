741b07538b50b3e9370544de24eb7ea78324149f
ahmed007tahir <ahmed007tahir@gmail.com>
2022-04-23 01:59:50 +0100

    Menu coming alive

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -9,10 +9,11 @@ List<int> intShare_1_2048 = ReadFile.Read_File("Share_1_2048.txt");
 List<int> intShare_2_2048 = ReadFile.Read_File("Share_2_2048.txt");
 List<int> intShare_3_2048 = ReadFile.Read_File("Share_3_2048.txt");
 
+List<int> list = new List<int>{};
+
+// get the user to choose the file
 while (true)
 {
-    
-
     Console.WriteLine(@"
     Please choose the file you wish to search:
     a) Share_1_256.txt      d) Share_1_2048.txt
@@ -21,38 +22,38 @@ while (true)
 
     Your Response: ");
 
-    string user_input = Console.ReadLine();
+    string file_choice = Console.ReadLine();
 
-    switch (user_input)
+    switch (file_choice)
     {
         case "a":
         {
-            Console.WriteLine("You picked a");
+            list = intShare_1_256;
             break;
         }
         case "b":
         {
-            Console.WriteLine("You picked b");
+            list = intShare_2_256;
             break;
         }
         case "c":
         {
-            Console.WriteLine("You picked c");
+            list = intShare_3_256;
             break;
         }
         case "d":
         {
-            Console.WriteLine("You picked d");
+            list = intShare_1_2048;
             break;
         }
         case "e":
         {
-            Console.WriteLine("You picked e");
+            list = intShare_2_2048;
             break;
         }
         case "f":
         {
-            Console.WriteLine("You picked f");
+            list = intShare_3_2048;
             break;
         }
         default:
@@ -61,4 +62,76 @@ while (true)
             break;
         }
     }
+
+while (true)
+{
+    Console.Write(@"
+    Please choose which algorithm you wish to use:
+    a) Bubble Sort           d) Quick Sort
+    b) Insertion Sort        e) Linear Search
+    c) Merge Sort            f) Binary Search
+
+    Your Response: ");
+
+    string algorithm_choice = Console.ReadLine();
+
+    switch (algorithm_choice)
+    {
+        case "a":
+        {
+            BubbleSort.BubbleSortAscending(list);
+            List<int> somelist = ReadFile.Get_Value(list);
+            foreach (var item in somelist)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        }
+        case "b":
+        {
+            List<int> somelist = InsertionSort.InsertionSortAscending(list);
+            somelist = ReadFile.Get_Value(list);
+            foreach (var item in somelist)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        }
+        case "c":
+        {
+            List<int> somelist = MergeSort.MergeSortAscending(list);
+            somelist = ReadFile.Get_Value(list);
+            foreach (var item in somelist)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        }
+        case "d":
+        {
+            QuickSort.Quick_Sort_Ascending(list,0,list.Count-1);
+            List<int> somelist = ReadFile.Get_Value(list);
+            foreach (var item in somelist)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        }
+        case "e":
+        {
+            break;
+        }
+        case "f":
+        {
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("Please try again!");
+            break;
+        }
+    }
+
+
+}
 }
